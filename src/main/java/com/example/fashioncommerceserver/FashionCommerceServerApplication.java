5416a102c3e8f2c303a1f944ca62164c67de3710
shindawull <juoiy05@naver.com>
2023-07-27 21:35:52 +0900

    코드리뷰 테스트

--- src/main/java/com/example/fashioncommerceserver/FashionCommerceServerApplication.java
+++ src/main/java/com/example/fashioncommerceserver/FashionCommerceServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FashionCommerceServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FashionCommerceServerApplication.class, args);
 	}

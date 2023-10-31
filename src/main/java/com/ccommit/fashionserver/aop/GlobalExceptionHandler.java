c9c2460839625f30efddbb20d8f374d345f31d76
shindawull <juoiy05@naver.com>
2023-11-01 03:07:01 +0900

    Refactor  : (issue/5) 주문 api 개발 - AOP 로그인체크 시 다중 타입일 경우 null로 로그인 안되는 문제 수정 - 에러코드,문구 추가 - 회원수정 시 회원일치 여부 추가 - log 어노테이션으로 통일 (서버 재시작할 때마다 붉은색으로 중복된다고 뜨는 문제 해결 목적)

--- src/main/java/com/ccommit/fashionserver/aop/GlobalExceptionHandler.java
+++ src/main/java/com/ccommit/fashionserver/aop/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ccommit.fashionserver.aop;
 
+import com.ccommit.fashionserver.exception.ErrorCode;
 import com.ccommit.fashionserver.exception.FashionServerException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * packageName    : com.ccommit.fashionserver.aop
  * fileName       : GlobalExceptionHandler
  * author         : juoiy
- * date           : 2023-09-07
+ * date           : 2023-09-07z
  * description    :
  * ===========================================================
  * DATE              AUTHOR             NOTE
@@ -25,9 +26,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = RuntimeException.class)
     private ResponseEntity<Object> handleNotContent(NullPointerException ex, HttpServletRequest request) {
-        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "ERR_000", ex.getMessage(), request.getServletPath());
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "ERR_000", ErrorCode.valueOf("INPUT_NULL_ERROR").getMessage(), request.getServletPath());
         return new ResponseEntity<>(commonResponse, new HttpHeaders(), commonResponse.getHttpStatus());
     }
+
     @ExceptionHandler(value = FashionServerException.class)
     private ResponseEntity<Object> handleDuplicate(FashionServerException ex, HttpServletRequest request) {
         CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, ex.status.toString(), ex.getMessage(), request.getServletPath());
--- src/main/java/com/ccommit/fashionserver/aop/LoginCheckAspect.java
+++ src/main/java/com/ccommit/fashionserver/aop/LoginCheckAspect.java
@@ -1,6 +1,9 @@
 package com.ccommit.fashionserver.aop;
 
+import com.ccommit.fashionserver.exception.ErrorCode;
+import com.ccommit.fashionserver.exception.FashionServerException;
 import com.ccommit.fashionserver.utils.SessionUtils;
+import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -24,6 +27,7 @@ import javax.servlet.http.HttpSession;
  */
 @Aspect
 @Component
+@Log4j2
 public class LoginCheckAspect {
     @Around("@annotation(com.ccommit.fashionserver.aop.LoginCheck) && @annotation(loginCheck)")
     public Object LoginSessionCheck(ProceedingJoinPoint proceedingJoinPoint, LoginCheck loginCheck) throws Throwable {
@@ -35,22 +39,33 @@ public class LoginCheckAspect {
             if (isLoginCheck == false) {
                 switch (loginCheck.types()[i].toString()) {
                     case "USER":
-                        id = SessionUtils.getUserLoginSession(session);
+                        try {
+                            id = SessionUtils.getUserLoginSession(session);
+                        } catch (NullPointerException e) {
+                            isLoginCheck = false;
+                        }
                         break;
                     case "SELLER":
-                        id = SessionUtils.getSellerLoginSession(session);
+                        try {
+                            id = SessionUtils.getSellerLoginSession(session);
+                        } catch (NullPointerException e) {
+                            isLoginCheck = false;
+                        }
                         break;
                     case "ADMIN":
-                        id = SessionUtils.getAdminLoginSession(session);
+                        try {
+                            id = SessionUtils.getAdminLoginSession(session);
+                        } catch (NullPointerException e) {
+                            isLoginCheck = false;
+                        }
                         break;
                 }
                 if (id != 0)
                     isLoginCheck = true;
             }
         }
-        if (isLoginCheck == false) {
-            throw new Exception("로그인이 필요합니다.");
-        }
+        if (isLoginCheck == false)
+            throw new FashionServerException(ErrorCode.valueOf("LOGIN_ERROR").getMessage(), 605);
         Object[] modifiedArgs = proceedingJoinPoint.getArgs();
         if (proceedingJoinPoint.getArgs() != null)
             modifiedArgs[index] = id;
